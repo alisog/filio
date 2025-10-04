3fc12a66d9b4ec6332dfa04cf78efed0571c7a54
developeralirezafullstack <developeralirezafullstack@gmail.com>
2025-10-04 21:40:17 +0330

    try initial google map

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
-<script setup>
-</script>
-
 <template>
-
+  <div id="map" style="width:100%;height:100vh"></div>
 </template>
 
-<style scoped>
+<script setup>
+import { onMounted, ref } from 'vue'
 
-</style>
+let map
+
+onMounted(async () => {
+  await window.google.maps.ready
+  const { Map } = await google.maps.importLibrary('maps')
+
+  initMap(Map)
+})
+
+function initMap(Map) {
+  map = new Map(document.getElementById('map'), {
+    center: { lat: 34.33, lng: -79.44 },
+    zoom: 16,
+    mapId: 'DEMO_MAP_ID'
+  })
+}
+</script>
